cd6583e7028d350e2455e03f81fc2b2072598b8a
cpedroni <44426472+cpedroni@users.noreply.github.com>
2021-12-03 11:23:31 +0100

    Update loginBean for Sql

--- src/main/java/ch/lab/unil/eventwebsite/beans/LoginBeans.java
+++ src/main/java/ch/lab/unil/eventwebsite/beans/LoginBeans.java
@@ -3,42 +3,52 @@ import ch.lab.unil.eventwebsite.Exceptions.DoesNotExistExeeption;
 import ch.lab.unil.eventwebsite.Database.database;
 import ch.lab.unil.eventwebsite.models.User;
 import java.io.Serializable;
+import java.util.List;
 //import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /** 
  *
  * @author valer
- 
+ */
+        
 @Named(value = "loginBean")
 @SessionScoped
 public class LoginBeans implements Serializable {
     
-    private String username="";
-    private String password="";
-    private User currentUser;
+    @PersistenceContext(unitName = "soar")
+    private EntityManager em;
+
+    private static User currentUser;
+    private String username = "";
+    private String password = "";
     
    // defaut constructor
     
-    public String logUserIn() {
-       
-            database db = database.getInstance();
-            User user = db.getUserByHisUserName(getUsername());
-            if(user != null && user.isPasswordCorrect(getPassword())){
-                // success log in ; user in the session 
-                setCurrentUser(user);
-
-                if(user.getUserRole().equalsIgnoreCase("seller")){
-                    return "/seller page/SellerHomePage.xhtml?faces-redirect=true";
-
-                }else{
-                    return "/customer page/CustomerHomePage.xhtml?faces-redirect=true";
-
-                }
-            }else{
-                return "/main page/Login.xhtml?faces-redirect=true"; 
+    public String userLogsIn() {
+        try {
+            User user = findByUsername();
+            if (user != null && user.isPasswordCorrect(password)) {
+                currentUser = user;
+                return "/UserPage/UserMainPage.xhtml?faces-redirect=true";
             }
+        } catch (DoesNotExistExeeption ex) {
+            System.out.println(ex.getMessage());
+        }
+        return "/MainPage/LoginPage.xhtml?faces-redirect=true";
+    }
+
+    protected User findByUsername() throws DoesNotExistExeeption {
+        Query query = em.createNamedQuery("User.findByUsername", User.class);
+        List<User> users = query.setParameter("username", username).getResultList();
+        if (users.size() > 0) {
+            return users.get(0);
+        }
+        throw new DoesNotExistExeeption("The user " + username + " does not exist.");
     }
 
     
@@ -69,8 +79,8 @@ public class LoginBeans implements Serializable {
     }
 
     
-    public  User getCurrentUser() {
-        return database.getInstance().getUserByHisUserName(username);
+    public User getCurrentUser() {
+        return currentUser;
     }
 
     
@@ -79,20 +89,20 @@ public class LoginBeans implements Serializable {
     }
 
 
-    public String resetPassword(){
-      User user = database.getInstance().getUserByHisUserName(username);
-        if(user != null){
-            if(database.getInstance().updateUserList(user, password) == true){
-               return "/main page/ResetPassword.xhtml?param=true&faces-redirect=true";
+    public String resetPassword() throws  DoesNotExistExeeption{
+        
+        User user = findByUsername();
+        if (user != null) {
+            if(password.length()>0){
+                user.setPassword(password);
+                return "/main page/ResetPassword.xhtml?param=true&faces-redirect=true";
             }else{
                 return "/main page/ResetPassword.xhtml?param=false&faces-redirect=true";
             }
-     
         }else{
             return "/main page/ResetPassword.xhtml?param=false&faces-redirect=true";
-        }    
-            
-        
+            } 
+        }
     }
-}
-*/
+
+
